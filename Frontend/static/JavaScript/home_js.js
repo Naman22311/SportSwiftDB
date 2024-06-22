9b7922cf9f77eb056791a9eef77974747843865d
shrutya22487 <shrutya22487@iiitd.ac.in>
2024-06-22 11:37:11 +0530

    update

---
+++ Frontend/static/JavaScript/home_js.js
@@ -0,0 +1,18 @@
+$(".go_up_1").on("mouseover", function () {
+    $(".go_up_1").stop().animate({
+        height: "220px"});
+});
+$(".go_up_1").on("mouseout", function() {
+    $(".go_up_1").stop().animate({
+        height: "150px"
+    });
+});
+$(".go_up_2").on("mouseover", function () {
+    $(".go_up_2").stop().animate({
+        height: "220px"});
+});
+$(".go_up_2").on("mouseout", function() {
+    $(".go_up_2").stop().animate({
+        height: "150px"
+    });
+});
---
+++ backend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__, template_folder='../Frontend/HTML')
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run()
